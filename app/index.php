23f5b634d7464c933b9150930e71c03e772f2df6
Jose Roman <jose@jromandesign.com>
2024-12-27 21:43:16 -0700

    folder app created

---
+++ app/index.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+
+<html>
+    <?php
+    include 'commons/head.php';
+    ?>
+
+    <body>
+        <?php
+        include 'commons/header.php';
+        echo '<div class="container pt-1">';
+        if(isset($_SESSION['user_id']) && $_SESSION['user_id'] != null){
+            if(!isset($_GET['loc'])){
+                include 'endpoints/muro.php';
+            }else if($_GET['loc']=='dash'){
+                include 'endpoints/escritorio.php';
+            }           
+        }else{
+            if(!isset($_GET['loc']) || $_GET['loc']=='login' || $_GET['loc']=='error'){
+                include 'endpoints/login.php';
+            }else{
+                include 'endpoints/register.php';
+            }
+            
+        }
+        echo '</div>';
+        include 'commons/footer.php';
+        ?>
+    </body>
+</html>
